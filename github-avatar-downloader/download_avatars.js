8512cb5b7bb30035cb99facc34f6a96cbe6ac480
Noah L <noah.luong@gmail.com>
2018-09-03 23:03:45 +0000

    download avatars project complete

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -1,32 +1,61 @@
 var request = require('request');
+var request2 = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function downloadImageByURL(url, filePath) {
+  var request = require('request');
+  var fs = require('fs');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-    cb(err, body);
-  });
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));               // Note 4
+
 }
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      "Authorization" : "token ff3e44af05036daf8e93a30db4c41df4e54cfff4"
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
+
+
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+
+function callback(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+
+  result.forEach(function(user) {
+    downloadImageByURL(user.avatar_url, `${user.login}.jpg`);
+  })
+}
+
+
+getRepoContributors(owner, repo, callback);
+
+// function requestURL(owner, repo) {
+// var options2 = {
+//     url: "https://api.github.com/repos/" + owner + "/" + repo + "/contributors"
+// }
+// }
+
+
